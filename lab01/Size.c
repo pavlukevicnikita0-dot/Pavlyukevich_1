7e7a60f454bfc0380dd59c1e27cb4114ce2e279b
pavlukevicnikita0-dot <pavlukevicnikita0@gmail.com>
2026-03-17 00:31:42 +0300

    Add files via upload

---
+++ lab01/Size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Вывод размеров типов данных с помощью sizeof
+    printf("char: %lu\n", sizeof(char));
+    printf("int: %lu\n", sizeof(int));
+    printf("float: %lu\n", sizeof(float));
+    printf("double: %lu\n", sizeof(double));
+    
+    return 0;
+}
