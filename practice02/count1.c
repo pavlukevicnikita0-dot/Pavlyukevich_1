6ed80fb1f84e91786ba1283e87ad97953203adc9
pavlukevicnikita0-dot <pavlukevicnikita0@gmail.com>
2026-03-18 17:31:32 +0300

    Add files via upload

---
+++ practice02/count1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+int N;
+int i;
+
+printf("Введите число: ");
+scanf("%d", &N);
+
+printf("Все числа от 0 до %d:\n", N); 
+for (i = 0; i <=N; i += 2) {
+printf("%d", i);
+}
+printf("\n");
+return 0;
+}
+
