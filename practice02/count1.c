510ab8bca02b6f2aa6dde75386c9a4ec0661ff8c
pavlukevicnikita0-dot <pavlukevicnikita0@gmail.com>
2026-03-18 17:41:24 +0300

    Update count1.c

--- practice02/count1.c
+++ practice02/count1.c
@@ -8,7 +8,7 @@ int i;
 printf("Введите число: ");
 scanf("%d", &N);
 
-printf("Все числа от 0 до %d:\n", N); 
+printf("Все четные числа от 0 до %d:\n", N); 
 for (i = 0; i <=N; i += 2) {
 printf("%d", i);
 }
