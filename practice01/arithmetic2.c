67bba97e958f5730c3f279c46ff1296d1336d39d
pavlukevicnikita0-dot <pavlukevicnikita0@gmail.com>
2026-03-20 00:50:57 +0300

    Add files via upload

---
+++ practice01/arithmetic2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main () {
+int a,b;
+float result;
+
+scanf("%d %d\n", &a, &b);
+
+result = (float)a / b;
+
+printf("%d %d = %.2f\n", a, b, result); 
+
+    return 0;
+}
+
+
+
+
+
+
+
+
